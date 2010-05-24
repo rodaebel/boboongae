583785ec79113c7cd5c879b3c3eeaf70230a7f7d
Tobias <tobias@LittleGeek.local>
2010-05-24 22:37:26 +0200

    Reverted last changes and moved GWT client to a new project.

--- src/boboongae/boboapp.py
+++ src/boboongae/boboapp.py
@@ -13,16 +13,14 @@ template_loader = chameleon.zpt.loader.TemplateLoader(
     auto_reload=os.environ.get('SERVER_SOFTWARE', '').startswith('Dev'))
 
 
-class BoboData(db.Model):
-    string = db.StringProperty()
-
-    def json(self):
-        return "{'string': %s}" % self.string
+class Note(db.Model):
+    author = db.UserProperty()
+    timestamp = db.DateTimeProperty(auto_now=True)
+    text = db.TextProperty()
 
 
 @bobo.query('/')
 def index():
-    BoboData(key_name="foobar", string="foobar").put()
     template = template_loader.load('index.html')
     user = users.get_current_user()
     return template(user=user)
